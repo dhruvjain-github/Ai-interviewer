804104a9329fae09b14d126046abb19ad0299934
Dhruv <dhruvdkjk@gmail.com>
2025-04-10 02:03:50 +0530

    Update route.tsx

--- app/api/getToken/route.tsx
+++ app/api/getToken/route.tsx
@@ -1,12 +1,9 @@
 import { AssemblyAI } from 'assemblyai'
 import { NextResponse } from 'next/server'
 
+const assemblyAi = new AssemblyAI({ apiKey: process.env.ASSEMBLY_API_KEY! });
 
-const assemblyAi = new AssemblyAI({apiKey:process.env.ASSEMBLY_API_KEY!})
-export async function GET(req: Request, res: Response) {
-   
-    
-    const token = await assemblyAi.realtime.createTemporaryToken({expires_in:4200})
-    return NextResponse.json(token)
-
+export async function GET(req: Request) {
+    const token = await assemblyAi.realtime.createTemporaryToken({ expires_in: 4200 });
+    return NextResponse.json(token);
 }
